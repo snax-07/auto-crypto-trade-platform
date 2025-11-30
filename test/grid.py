0e9260c0ee71ccefb8854ba1c2bb8af2f0b61b85
Swapnil Nade <141449289+snax-07@users.noreply.github.com>
2025-11-30 21:43:10 +0530

    Delete test directory

--- test/grid.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-class grid:
-    def __init__(self , config):
-        self.config = config
-    
-    def runBot(self):
-        while True:
-            print(f'bot for {self.config} in grid')
--- test/swing.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-class swing:
-    def __init__(self , config):
-        self.config = config
-    
-    def runBot(self):
-        while True:
-            print(f'bot for {self.config} in swing')
